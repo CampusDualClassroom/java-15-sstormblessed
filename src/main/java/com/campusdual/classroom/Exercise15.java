2f4f4e2bd30795733a4f02145da8c4ad230cb199
Nicolás Vázquez <nikovazquez52@gmail.com>
2024-09-26 13:11:09 +0200

    ejercicio completo

--- src/main/java/com/campusdual/classroom/Exercise15.java
+++ src/main/java/com/campusdual/classroom/Exercise15.java
@@ -4,12 +4,12 @@ import java.util.Calendar;
 
 public class Exercise15 {
 
-	public static void main(String[] args) {
-		FreshMerchandise fm = new FreshMerchandise("Manzanas", "001-9", "Frutería de ayer S.L.", 8, "C", "114D", 53,
-				Calendar.getInstance().getTime());
-		fm.printSpecificData();
-		System.out.println(fm.getLocation());
+    public static void main(String[] args) {
+        FreshMerchandise fm = new FreshMerchandise("Manzanas", "001-9", "Frutería de ayer S.L.", 8, "C", "114D", 53,
+                Calendar.getInstance().getTime());
+        fm.printSpecificData();
+        System.out.println(fm.getLocation());
 
-	}
+    }
 
 }
--- src/main/java/com/campusdual/classroom/FreshMerchandise.java
+++ src/main/java/com/campusdual/classroom/FreshMerchandise.java
@@ -4,5 +4,28 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class FreshMerchandise extends Merchandise {
+    public Date expirationDate;
 
+    public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf, int quantity, Date expirationDate) {
+        super(name, uniqueId, responsibleId, zone, area, shelf, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    @Override
+    public Object getSpecificData() {
+        return getFormattedDate(expirationDate);
+    }
+
+    public void printSpecificData() {
+        System.out.println("Expiration date: " + getFormattedDate(expirationDate));
+    }
+
+    public String getFormattedDate(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        return sdf.format(date);
+    }
 }
